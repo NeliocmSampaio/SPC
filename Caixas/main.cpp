7c0c2e0874a1022827cd0c7a3faf5ba2df39fcf0
Nélio <neliocmsampaio@gmail.com>
2017-05-11 20:09:23 -0300

    Caixas completo

--- Caixas/main.cpp
+++ Caixas/main.cpp
@@ -1,3 +1,16 @@
+/*
+ * Nome: Nélio Cezar Muniz Sampaio
+ * Matricula: 2015042150
+ * Codigo do Problema: CAIXAS   Problema: Desempilhando Caixas
+ *
+ * O algoritmo, primeiramente, salva a altura de cada pilha pelo número de caixas de cada uma.
+ * Depois, conta o número de caixas acima da caixa 1 nas pilhas entre a última pilha com altura
+ * menor que a altura da caixa 1 e a pilha anterior à pilha que contém a caixa 1, a soma dessas
+ * caixas com o número de caixas acima da caixa 1 é a dificuldade de desempilhá-la pelo lado
+ * esquerdo. O processe é repetido para o lado direito, e a resposta é a menor entre a dificuldade
+ * do lado esquerdo e do lado direito.
+ * */
+
 #include <iostream>
 
 int main() {
@@ -22,34 +35,36 @@ int main() {
                 scanf("%d", &caixa);
                 if(caixa == 1)
                 {
-                    pilha_c1 = i+1;
-                    altura_c1 = j;
+                    pilha_c1 = i;
+                    altura_c1 = j+1;
                 }//if
             }//for
             alturas[i] = caixas_na_pilha;
         }//for
 
-        for(int i=0; i<=pilha_c1; i++)
+        for(int i=0; i<pilha_c1; i++)
         {
-            if(alturas[i]>altura_c1)
+            if(alturas[i]>=altura_c1)
             {
-                soma_alturas_dir+=alturas[i]-altura_c1;
+                soma_alturas_dir+=alturas[i]+1-altura_c1;
             }else
             {
                 soma_alturas_dir = 0;
             }
         }//for
 
-        for(int i=pilha_c1; i<p; i++)
+        for(int i=pilha_c1+1; i<p; i++)
         {
-            if(alturas[i]>altura_c1)
+            if(alturas[i]>=altura_c1)
             {
-                soma_alturas_esq+=alturas[i]-altura_c1;
+                soma_alturas_esq+=alturas[i]+1-altura_c1;
             }else
             {
                 break;
             }
         }//for
+        soma_alturas_dir += alturas[pilha_c1] - altura_c1;
+        soma_alturas_esq += alturas[pilha_c1] - altura_c1;
 
         printf("%d\n", soma_alturas_dir<soma_alturas_esq ?
                        soma_alturas_dir : soma_alturas_esq);
